0cf10f8ecdcc5fdd6725eb6d41044ade29ae2f7d
Lumilebest <lumilebest@gmail.com>
2026-09-17 19:15:17 +0200

    refactor fonctionnel

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,23 +1,26 @@
+#include "entity/movingEntity/MovingEntity.hpp"
 #include <SDL3/SDL.h>
-#include "entity/moving/movingEntity.hpp"
-#include "map/map.hpp"
-#include "wave/wave.hpp"
+#include <SDL3/SDL_mouse.h>
 #include <SDL3/SDL_power.h>
+#include <SDL3/SDL_rect.h>
 #include <SDL3/SDL_render.h>
 #include <SDL3/SDL_stdinc.h>
-#include <iostream>
+#include <SDL3/SDL_timer.h>
 
 int main() {
     SDL_Window* win = SDL_CreateWindow("Fenêtre", 800, 600, SDL_WINDOW_RESIZABLE);
     SDL_Renderer* ren = SDL_CreateRenderer(win, nullptr);
 
+    MovingEntity e(50,50,50,50,1500);
 
-    Wave wave;
+    SDL_FPoint mouse;
 
     bool running = true;
     SDL_Event event;
 
+    Uint64 lastTime = SDL_GetTicks();
     Uint64 time;
+    float delta = 0.0f;
 
     while (running) {
         while (SDL_PollEvent(&event)) {
@@ -25,11 +28,18 @@ int main() {
             if (event.type == SDL_EVENT_KEY_DOWN && event.key.key == SDLK_ESCAPE) running = false;
         }
         time = SDL_GetTicks();
+        delta = (time-lastTime)/1000.0f;
 
-        SDL_SetRenderDrawColor(ren, 0,255,255,255);
+        SDL_GetMouseState(&mouse.x, &mouse.y);
+
+        e.move(delta, &mouse);
+
+        SDL_SetRenderDrawColor(ren, 0,255,0,255);
         SDL_RenderClear(ren);
-        e1.draw(ren);
+        e.drawCollide(ren);
+
         SDL_RenderPresent(ren);
+        lastTime = time;
     }
 
     SDL_DestroyRenderer(ren);
--- src/entity/moving/movingEntity.cpp
+++
@@ -1,9 +0,0 @@
-#include "entity/moving/movingEntity.hpp"
-#include "entity/entity.hpp"
-
-MovingEntity::MovingEntity(float x, float y, float w, float h, float speed) : Entity(x,y,w,h), speed(speed){
-}
-
-void MovingEntity::move(){
-    setX(getX()+speed);
-}
